b3f4ab61eee189aa4c5fa9fdc4e7e9d765fc404f
Live session user <aferrei3@binghamton.edu>
2022-11-11 22:58:39 +0000

    fixed setup for npm and added gitignore

--- pyFiles/setup.py
+++ pyFiles/setup.py
@@ -4,8 +4,6 @@ import os
 os.system('sudo apt-get update')
 os.system('sudo apt install whois')
 os.system('sudo apt install curl')
-os.system('sudo apt install npm')
-
 
 #installing pip
 os.system('sudo apt-get install software-properties-common')
@@ -23,13 +21,19 @@ os.system('virtualenv googenv')
 os.system('googenv/Scripts/activate')
 os.system('googenv/Scripts/pip install google-cloud-domains')
 
-#npm initialization
-os.system('npm init -y')
-os.system('npm i body-parser mongoose express')
-
 #google cloud install & init
 os.system('sudo apt-get install apt-transport-https ca-certificates gnupg')
 os.system('echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list')
 os.system('curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/cloud.google.gpg')
 os.system('sudo apt-get update && sudo apt-get install google-cloud-cli')
 os.system('gcloud init')
+
+
+
+os.system('sudo apt install npm')
+
+#npm initialization
+os.system('npm init -y')
+os.system('npm i body-parser mongoose express')
+
+
